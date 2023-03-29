c0043d19e9fb7b249506f535452fe012478acebe
Stanislavcod <stas.dryk@mail.ru>
2023-03-29 22:12:25 +0300

    create project

---
+++ Library.Model/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryTestApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ LibraryTestApp/Configuration/ConfigurationHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace LibraryTestApp.Configuration
+{
+    public class ConfigurationHelper
+    {
+
+    }
+}
