143eec3ec7689d8a7a96f46ec400de8ea67a20ca
Stanislavcod <stas.dryk@mail.ru>
2023-04-03 22:12:56 +0300

    add ApplicationDatabaseContext

---
+++ Library.Model/DatabaseContext/ApplicationDatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿
+using Library.Model.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Model.DatabaseContext
+{
+    class ApplicationDatabaseContext : DbContext
+    {
+        public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
